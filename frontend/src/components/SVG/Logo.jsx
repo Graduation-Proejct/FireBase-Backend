17a222ac20564d442c10b2f06e4dc0291529e442
SayedKhaledd <63265214+SayedKhaledd@users.noreply.github.com>
2022-05-10 21:05:06 +0200

    edit

--- frontend/src/components/SVG/Logo.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { ReactComponent as StarLogo } from "../../assets/svgs/logo.svg";
-export default function Logo() {
-  return (
-    <>
-      <StarLogo />
-    </>
-  );
-}
